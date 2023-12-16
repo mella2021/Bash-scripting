aa311e64cd76f1e7e1207131a8d733230e863287
mella <mellabae322@gmail.com>
2023-12-16 04:57:09 -0500

    changes again

---
+++ fortune.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Program to tell a person a fortune
+echo -e "\n~~ Fortune Teller ~~\n"
+RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+N=$(( RANDOM % 6))
+GET_FORTUNE() {
+    echo Ask a yes or no question:
+    read QUESTION
+}
+until [[ $QUESTION == test? ]]
+do
+GET_FORTUNE
+done
+echo ${RESPONSES[N]}
